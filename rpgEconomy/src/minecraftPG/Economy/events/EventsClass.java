11206a43d77327a3d19dc48cb8c4b53354758930
HalfLegend <geoffroy.calon@laposte.net>
2018-11-09 00:29:40 +0100

    First implementation of a shop
    - Adding the possibility to create a shop using Wall_Sign.
    To create a shop:
    - [Buy]
    - [Amount]
    - [Price]
    - [Item]
    
    Example:
    [Buy]
    10
    50
    Stone

--- rpgEconomy/src/minecraftPG/Economy/events/EventsClass.java
+++ rpgEconomy/src/minecraftPG/Economy/events/EventsClass.java
@@ -1,9 +1,15 @@
 package minecraftPG.Economy.events;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import minecraftPG.Economy.EconomyManager;
@@ -30,5 +36,57 @@ public class EventsClass implements Listener {
             EconomyManager.setBalance(player.getName(), 0);
         }
         
+    }
+    
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+         Action action = event.getAction();
+         Block block = event.getClickedBlock();
+         Player player = event.getPlayer();
+
+         
+         //Actions on right click.
+         if (action.equals(Action.RIGHT_CLICK_BLOCK)) {
+             if (block.getType() == Material.WALL_SIGN) {
+                 Sign sign = (Sign) block.getState();
+                 if(sign.getLine(0).equalsIgnoreCase("[Buy]")) {
+                     int quantity = 0;
+                     try {
+                         quantity =  Integer.parseInt(sign.getLine(1));
+                     } catch (Exception e) {
+                         player.sendMessage(ChatColor.RED + "The quantityy is not an integer");
+                         //NOTHING TO DO
+                     }
+                     if (quantity > 0 && quantity <= 64) {
+                         double price = 0;
+                         try {
+                             price = Double.parseDouble(sign.getLine(2));
+                         } catch (Exception e) {
+                             player.sendMessage(ChatColor.RED + "The price is not a double");
+                             //NOTHING TO DO
+                         }
+                         if (price != 0) {
+                             Material onSell = Material.getMaterial(sign.getLine(3).toUpperCase());
+                             if(onSell != null) {
+                                 if (EconomyManager.hasEnoughtMoney(player.getName(), price)) {
+                                     giveItem(player, quantity, onSell);
+                                     EconomyManager.add(player.getName(), -price);
+                                     player.sendMessage(ChatColor.DARK_GRAY + "You bought " + ChatColor.GREEN + quantity + " " 
+                                     + onSell.toString() + ChatColor.DARK_GRAY + " for " + ChatColor.RED + price + "$");
+                                 } else {
+                                     player.sendMessage(ChatColor.RED + "You don't have enought money to do this.");
+                                 }
+                             }
+                         }
+                     }
+                 }
+             }
+         }
+    }
+
+    private void giveItem(Player player, int quantity, Material onSell) {
+        ItemStack item = new ItemStack(onSell);
+        item.setAmount(quantity);
+        player.getInventory().addItem(item);
     }
 }
